342f5719365e4576bbf1c6a7743a6395c46d9872
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-04-09 11:04:39 +0800

    publish, now can conf three dir 1. www :client root dir(include *.html *.js *.css etc.) 2. goahead:srv dir 3. out :out put dir, a tar.gz file 4. README file (optional)

--- publish.sh
+++ publish.sh
@@ -12,51 +12,50 @@
 major=`cat version.h |grep -i major|awk -F ' ' '{print $3}'`
 minor=`cat version.h |grep -i minor|awk -F ' ' '{print $3}'`
 patchlevel=`cat version.h |grep -i patchlevel|awk -F ' ' '{print $3}'`
-#前端根目录
-www="/home/lee/Aptana Studio 3 Workspace/www"
-#
-echo "into www"
+#前端根目录 www
+www='/home/zodiac1111/Aptana Studio 3 Workspace/www/'
+#gohead  root dir a 
+goahead_dir='/home/zodiac1111/workspace/goahead'
+#output dir
+out='/samba_folder'
+#project name : webs
+pro='webs'
+
+#cd "$output_dir"
+echo "www dir = \"$www \""
+echo "goahead_dir = \"$goahead_dir\""
+
+
+echo "* Go into \"$goahead_dir\" and make update..."
 cd "$www" && ./makeversion.sh 
 
 if [ r"$1" = r"dev" ] ; then
 	#用于软件开发的版本
-	filename=webs-$major.$minor.$patchlevel-dev.tar.gz 
+	filename=$pro-$major.$minor.$patchlevel-dev.tar.gz 
 	echo "Filename:"$filename "正在打包[dev版本](packing)..."
-	cd /samba_folder \
+	cd $out \
 	&& tar -zcf  $filename \
-       		-C '/home/lee/Aptana Studio 3 Workspace/' www  \
-		-C '/home/lee/workspace' goahead \
-		-C '/home/lee/workspace' README.txt \
+	       	-C "$www/.." www \
+		-C "$goahead_dir/.." goahead \
+		-C "$goahead_dir/.." README.txt \
 	&& echo -e \
-		"\e[32m[成功]\e[0m 生成文件:\e[31m`pwd`/$filename\e[0m"
+		"\e[32m[成功]\e[0m 生成文件:\e[32m`pwd`/$filename\e[0m"
 else
 	#用于安装使用的版本
 	#使用类似 --exclude="*.[ch]" \ 的语句排除不需要打开的文件.
-	filename=webs-$major.$minor.$patchlevel.tar.gz 
+	filename=$pro-$major.$minor.$patchlevel.tar.gz 
 	echo "Filename:"$filename "正在打包[发布版本](packing)..."
-	cd /samba_folder \
+	cd $out \
 	&& tar --exclude=".*" \
 		--exclude="*~" \
 		--exclude="*.o" \
 		--exclude="*.a" \
 		--exclude="*.log" \
 	       	-zcf  $filename \
-	       	-C '/home/lee/Aptana Studio 3 Workspace/' www \
-		-C '/home/lee/workspace' goahead \
-		-C '/home/lee/workspace' README.txt \
+	       	-C "$www/.." www \
+		-C "$goahead_dir/.." goahead \
+		-C "$goahead_dir/.." README.txt \
 	&& echo -e \
-		"\e[32m[成功]\e[0m 生成文件:\e[31m`pwd`/$filename\e[0m"
+		"\e[32m[成功]\e[0m 生成文件:\e[32m`pwd`/$filename\e[0m"
 fi
 exit 0
-
-
-
-#下面的暂时保留,但是不使用.
-echo "Filename:"$filename "正在打包(packing)..."
-cd /samba_folder \
-&& tar --exclude .git* -jcf  $filename \
-       	-C '/home/lee/Aptana Studio 3 Workspace/' wwwdemo  \
-	-C '/home/lee/workspace' goahead \
-	-C '/home/lee/workspace' README.txt index.html \
-&& echo -e \
-"\e[32m[成功]\e[0m 生成文件:\e[31m`pwd`/$filename\e[0m"
