44f935ec0faecb597f466c3ea8623c2a24e9df0a
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-04-12 12:20:48 +0800

    实时数据准备工作, 编译结构标准化

---
+++ LINUX/rtit.c
@@ -0,0 +1,7 @@
+/**
+ * @file rtit.c real time 电量,实时电量
+ */
+#include "rtit.c"
+int f1(void){
+	return 0;
+}
---
+++ LINUX/rtit.h
@@ -0,0 +1,8 @@
+/**
+ * @file rtit.h real time 电量,实时电量
+ */
+#ifndef _RTIT_H_
+#define _RTIT_H_
+#include "struct.h"
+#include "externCShm.h"
+#endif
